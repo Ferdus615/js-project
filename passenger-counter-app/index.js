04dde71e26d94f61c10dcfee42b6f947e18449d9
Ferdus615 <ferdus615@gmail.com>
2025-06-03 22:01:26 +0600

    counter app done

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -4,3 +4,22 @@ const increment = () => {
   count += 1;
   document.getElementById("counter").innerText = count;
 };
+
+const decrement = () => {
+  if (count > 0) {
+    count -= 1;
+    document.getElementById("counter").innerText = count;
+  }
+};
+
+const save = () => {
+  const savedCounts = document.getElementById("savedCounts");
+  const newCounts = document.createElement("li");
+  newCounts.innerText = count;
+  savedCounts.appendChild(newCounts);
+};
+
+const reset = () => {
+  count = 0;
+  document.getElementById("counter").innerText = count;
+};
