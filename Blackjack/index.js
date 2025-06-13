5340e0be234fb260df7e5e833a3ba5cf751e3db6
Ferdus615 <ferdus615@gmail.com>
2025-06-13 22:07:03 +0600

    game basic logic done

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -2,53 +2,75 @@ const randomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + 1);
 };
 
+const generatedCard = [];
+let totalValue = 0;
+
 const showCard = document.getElementById("showCard");
-const dealcard = () => {
+const trackCard = document.getElementById("trackCard");
+
+const dealCard = () => {
   const newCard = randomNumber(1, 13);
   showCard.textContent = newCard;
 
+  //   const allDealtCard = generatedCard.push(newCard);
+  //   trackCard.textContent = allDealtCard; //shows the number of cards dealt, not the cards themselves
+  trackCard.textContent = generatedCard.join(", "); // shows the dealt cards
+  console.log(generatedCard);
+
+  totalValue += newCard;
+  console.log(totalValue);
+
+  if (totalValue === 21) {
+    trackCard.textContent = "Blackjack!";
+  } else if (totalValue > 21) {
+    trackCard.textContent = "Bust!";
+    totalValue = 0; // Reset totalValue after bust
+    generatedCard.length = 0; // Clear the dealt cards
+    showCard.textContent = ""; // Clear the displayed card
+  }
+
   switch (newCard) {
     case 1:
-      showCard.textContent = "A";
+      generatedCard.push((showCard.textContent = "A"));
       break;
     case 2:
-      showCard.textContent = "2";
+      generatedCard.push((showCard.textContent = "2"));
       break;
     case 3:
-      showCard.textContent = "3";
+      generatedCard.push((showCard.textContent = "3"));
       break;
     case 4:
-      showCard.textContent = "4";
+      generatedCard.push((showCard.textContent = "4"));
       break;
     case 5:
-      showCard.textContent = "5";
+      generatedCard.push((showCard.textContent = "5"));
       break;
     case 6:
-      showCard.textContent = "6";
+      generatedCard.push((showCard.textContent = "6"));
       break;
     case 7:
-      showCard.textContent = "7";
+      generatedCard.push((showCard.textContent = "7"));
       break;
     case 8:
-      showCard.textContent = "8";
+      generatedCard.push((showCard.textContent = "8"));
       break;
     case 9:
-      showCard.textContent = "9";
+      generatedCard.push((showCard.textContent = "9"));
       break;
     case 10:
-      showCard.textContent = "10";
+      generatedCard.push((showCard.textContent = "10"));
       break;
     case 11:
-      showCard.textContent = "J";
+      generatedCard.push((showCard.textContent = "J"));
       break;
     case 12:
-      showCard.textContent = "Q";
+      generatedCard.push((showCard.textContent = "Q"));
       break;
     case 13:
-      showCard.textContent = "K";
+      generatedCard.push((showCard.textContent = "K"));
       break;
     default:
-      showCard.textContent = "Error";
+      generatedCard.push((showCard.textContent = "Error"));
       break;
   }
 };
