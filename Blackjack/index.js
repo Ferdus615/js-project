da6a8d4a5d508dcecb862ca33b56c24c8e36a716
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-06-15 19:31:15 +0600

    new logic on display card symbol, blackjack//busted, button disable(deal)

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -5,6 +5,8 @@ const randomNumber = (min, max) => {
 const generatedCard = [];
 let totalValue = 0;
 let hasBlakJack = false;
+let hasBusted = false;
+let isGameStarted = false;
 
 const showCard = document.getElementById("showCard");
 const trackCard = document.getElementById("trackCard");
@@ -22,8 +24,7 @@ const resetGame = () => {
   dealCard.disabled = false; // Enable the deal card button
 };
 
-// Card dealing button
-const dealCardBtn = () => {
+const deal = () => {
   const newCard = randomNumber(1, 13); // Generate a random card value between 1 and 13
   showCard.textContent = newCard;
 
@@ -45,10 +46,34 @@ const dealCardBtn = () => {
       break;
   }
 
-  totalValue += newCard; // Add the value of the new card to totalValue
-  console.log(totalValue);
-
   trackCard.textContent = generatedCard.join(", "); // shows the dealt cards
   console.log(generatedCard);
 
+  totalValue += newCard; // Add the value of the new card to totalValue
+  console.log(totalValue);
+};
+
+// Card dealing button
+const dealCardBtn = () => {
+  if (isGameStarted) {
+    deal();
+    if (totalValue === 21) {
+      hasBlakJack = true; // Set the blackjack flag
+      alert("Blackjack! You win!");
+      dealCard.disabled = true; // Disable the deal card button
+    } else if (totalValue > 21) {
+      hasBusted = true;
+      alert("Busted! Total value exceeded 21.");
+    } else {
+      alert(`Current total value: ${totalValue}`);
+    }
+  } else {
+    deal();
+    deal();
+    isGameStarted = true; // Set the game as started
+  }
+
+  if (hasBusted || hasBlakJack) {
+    dealCard.disabled = true; // Disable the deal card button if the game is over
+  }
 };
