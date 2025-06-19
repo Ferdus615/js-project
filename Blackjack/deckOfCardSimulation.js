291baf672df78b2b307c9ba8a62b9897ec20886f
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-06-19 12:06:16 +0600

    real card simulation logic testing

---
+++ Blackjack/deckOfCardSimulation.js
@@ -0,0 +1,68 @@
+const deckOfCard = [];
+
+const suits = ["♧", "♢", "♡", "♤"];
+const ranks = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+const values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+
+// const createDeck = () => {
+//   for (let suit of suits) {
+//     for (let rank of ranks) {
+//       deckOfCard.push(`${rank} of ${suit}`);
+//     }
+//   }
+// };
+
+const createDeck = () => {
+  for (let suitsIndex = 0; suitsIndex < suits.length; suitsIndex++) {
+    for (let ranksIndex = 0; ranksIndex < ranks.length; ranksIndex++) {
+      const card = {
+        suit: suits[suitsIndex],
+        rank: ranks[ranksIndex],
+        value: values[ranksIndex],
+      };
+      deckOfCard.push(`${card.rank} of ${card.suit}`);
+    }
+  }
+};
+createDeck();
+
+// console.log(deckOfCard);
+// console.log(deckOfCard.length);
+
+const pickedCard = [];
+
+const randomCardPicker = () => {
+  Math.floor(Math.random() * (suits.length * ranks.length));
+};
+
+randomCardPicker();
+randomCardPicker();
+randomCardPicker();
+randomCardPicker();
+randomCardPicker();
+randomCardPicker();
+randomCardPicker();
+randomCardPicker();
+randomCardPicker();
+randomCardPicker();
+randomCardPicker();
+
+pickedCard.push(randomCardPicker);
+console.log(`pickedCard: ${pickedCard}`);
+
+const drawCard = deckOfCard[randomCardPicker];
+console.log(drawCard);
