32c61e613918f71e7913258ad48efc381ede8258
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-10-25 23:22:24 +0600

    added apple generation logic in random place

--- SnakeGame/game.js
+++ SnakeGame/game.js
@@ -1,15 +1,39 @@
+// the canvas(paper) and the context(pen)
 const canvas = document.getElementById("gameBoard");
 const ctx = canvas.getContext("2d");
 
+let direction = "RIGHT";
+let speed = 500;
+
+// each section of the gameBoard
 const tileSize = 20;
 const gap = 1;
 const segmentSize = tileSize - gap * 2;
 
 const snake = [
-  { x: 6 * tileSize, y: 15 * tileSize },
-  { x: 5 * tileSize, y: 15 * tileSize },
-  { x: 4 * tileSize, y: 15 * tileSize },
+  { x: 14 * tileSize, y: 15 * tileSize },
+  { x: 13 * tileSize, y: 15 * tileSize },
+  { x: 12 * tileSize, y: 15 * tileSize },
 ];
+
+const drawApple = () => {
+  const pX = Math.floor(Math.random() * 30);
+  const pY = Math.floor(Math.random() * 30);
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(pX * tileSize, pY * tileSize, segmentSize, segmentSize);
+};
+
+const moveSnake = () => {
+  const head = { ...snake[0] };
+
+  if (direction === "RIGHT") {
+    head.x += tileSize;
+  }
+
+  snake.unshift(head);
+  snake.pop();
+};
 
 const drawSnake = () => {
   snake.forEach((segment, index) => {
@@ -22,4 +46,11 @@ const drawSnake = () => {
   });
 };
 
-drawSnake();
+const drawGame = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  moveSnake();
+  drawApple();
+  drawSnake();
+};
+
+setInterval(drawGame, speed);
